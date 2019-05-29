5a0a793da8eedf309a92c797ac274a37a9b73edd
Daniel Tigse <danieltp15@hotmail.com>
2019-05-29 17:45:23 -0500

    visual changes

--- js/main.js
+++ js/main.js
@@ -20,6 +20,10 @@
         6: 'brown'
       };
 
+  var minutesLabel = document.getElementById("minutes");
+  var secondsLabel = document.getElementById("seconds");
+  var totalSeconds = 0;
+
   function gameSetup () {
     generateSecretCode(1, 7);
 
@@ -29,6 +33,24 @@
 
     document.getElementById('newGame').onclick = newGame;
     document.getElementById('delete').onclick = deleteLast;
+
+    totalSeconds = 0;
+    setInterval(setTime, 1000);
+  }
+
+  function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  }
+
+  function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
   }
 
   function insertGuess () {
